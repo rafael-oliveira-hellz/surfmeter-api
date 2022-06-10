29c18760b00ccbec2ef28003379202030b7e1595
Rafael de Oliveira Silva <95747012+rafael-oliveira-hellz@users.noreply.github.com>
2022-06-10 14:21:06 -0300

    + Create beach and user models, and the web component

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -2,8 +2,10 @@ import { StormGlass } from '@src/clients/stormGlass';
 import stormglassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 import * as stormglassWeatherPointFixture from '@test/fixtures/stormglass_weather_3_hours.json';
 import * as HTTPUtil from '@src/util/request';
+import CacheUtil from '@src/util/cache';
 
 jest.mock('@src/util/request');
+jest.mock('@src/util/cache');
 
 describe('StormGlass client', () => {
   /**
@@ -12,6 +14,7 @@ describe('StormGlass client', () => {
   const MockedRequestClass = HTTPUtil.Request as jest.Mocked<
     typeof HTTPUtil.Request
   >;
+  const MockedCacheUtil = CacheUtil as jest.Mocked<typeof CacheUtil>;
   /**
    * Used for instance method's mocks
    */
@@ -24,7 +27,9 @@ describe('StormGlass client', () => {
       data: stormglassWeatherPointFixture,
     } as HTTPUtil.Response);
 
-    const stormGlass = new StormGlass(mockedRequest);
+    MockedCacheUtil.get.mockReturnValue(undefined);
+
+    const stormGlass = new StormGlass(mockedRequest, MockedCacheUtil);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormglassNormalizedResponseFixture);
   });
@@ -46,10 +51,27 @@ describe('StormGlass client', () => {
       data: incompleteResponse,
     } as HTTPUtil.Response);
 
-    const stormGlass = new StormGlass(mockedRequest);
+    MockedCacheUtil.get.mockReturnValue(undefined);
+
+    const stormGlass = new StormGlass(mockedRequest, MockedCacheUtil);
     const response = await stormGlass.fetchPoints(lat, lng);
 
     expect(response).toEqual([]);
+  });
+
+  it('should get the normalized forecast points from cache and use it to return data points', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    mockedRequest.get.mockResolvedValue({
+      data: null,
+    } as HTTPUtil.Response);
+
+    MockedCacheUtil.get.mockReturnValue(stormglassNormalizedResponseFixture);
+
+    const stormGlass = new StormGlass(mockedRequest, MockedCacheUtil);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stormglassNormalizedResponseFixture);
   });
 
   it('should get a generic error from StormGlass service when the request fail before reaching the service', async () => {
@@ -58,14 +80,16 @@ describe('StormGlass client', () => {
 
     mockedRequest.get.mockRejectedValue('Network Error');
 
-    const stormGlass = new StormGlass(mockedRequest);
+    MockedCacheUtil.get.mockReturnValue(undefined);
+
+    const stormGlass = new StormGlass(mockedRequest, MockedCacheUtil);
 
     await expect(stormGlass.fetchPoints(lat, lng)).rejects.toThrow(
       'Unexpected error when trying to communicate to StormGlass: "Network Error"'
     );
   });
 
-  it('should get a StormGlassResponseError when the StormGlass service responds with error', async () => {
+  it('should get an StormGlassResponseError when the StormGlass service responds with error', async () => {
     const lat = -33.792726;
     const lng = 151.289824;
 
